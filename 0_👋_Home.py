03092522ecd6c52d8ae4d2d45ebe8780b5aa53d5
Your Name <you@example.com>
2023-12-20 17:52:09 +0100

    git add . git commit -m "123" git push

--- 0_👋_Home.py
+++ 0_👋_Home.py
@@ -20,11 +20,11 @@ with st.sidebar:
 
 
 # ----- Top title -----
-st.write(f"""<div style="text-align: center;"><h1 style="text-align: center;">👋 Hi! My name is ????</h1></div>""", unsafe_allow_html=True)  # TODO: Add your name
+st.write(f"""<div style="text-align: center;"><h1 style="text-align: center;">👋 Hi! My name is Irina</h1></div>""", unsafe_allow_html=True)  # TODO: Add your name
 
 
 # ----- Profile image file -----
-profile_image_file_path = "profile.png"       # TODO: Upload your profile image to the same folder as this script and update this if it has a different name
+profile_image_file_path = "IM.jpg"       # TODO: Upload your profile image to the same folder as this script and update this if it has a different name
 
 with open(profile_image_file_path, "rb") as img_file:
     img = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
@@ -39,7 +39,7 @@ st.write(f"""
 
 
 # ----- Personal title or short description -----
-current_role = "Current Role and/or Studies or Description"   # TODO: Change this
+current_role = "Student"   # TODO: Change this
 
 st.write(f"""<div style="text-align: center;"><h4><i>{current_role}</i></h4></div>""", unsafe_allow_html=True)
 
@@ -51,17 +51,17 @@ st.subheader("About Me")
 
 # TODO: Modify and adapt the following lines to your info, you can add or remove some details if you want
 st.write("""
-- 🧑‍💻 I am a <Current Role and/or Studies> 
+- 🧑‍💻 I am a Student
 
-- 🛩️ prev: <Previous Experience, Background or Studies>
+- 🛩️ prev: Worked as IT manager
 
-- ❤️ <Your Passion and/or Interests>
+- ❤️ Fast drive
 
-- 🤖 <Your Personal Projects>
+- 🤖 Don't have yet
 
-- 🏂 <Your Hobbies>
+- 🏂 Padel
 
-- 📫 How to reach me: <Your Email>
+- 📫 How to reach me: irinamik1@hotmail.com
 
 - 🏠 Barcelona
 """)
